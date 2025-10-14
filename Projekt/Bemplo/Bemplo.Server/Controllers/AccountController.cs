0005e378d24ec9f26a5e41d52e92702c19a39a11
HonzikVacik <149482083+HonzikVacik@users.noreply.github.com>
2025-10-14 10:36:53 +0200

    Registrace účtu

--- Projekt/Bemplo/Bemplo.Server/Controllers/AccountController.cs
+++ Projekt/Bemplo/Bemplo.Server/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bemplo.Server.Models;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +11,77 @@ namespace Bemplo.Server.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        // GET: api/<AccountController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
         {
-            return new string[] { "value1", "value2" };
+            _context = context;
         }
 
-        // GET api/<AccountController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<AccountController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Register(byte accountType, string name, string surname, byte sexType, DateTime date, string email, string password, string country, string region, string city, string address, string description, bool agreeWithPrivacyPolicy)
         {
-        }
+            if (!ValidityControl.CheckNewAccount(accountType, name, surname, sexType, date, email, password, country, region, city, address, description, agreeWithPrivacyPolicy))
+                return BadRequest("Neplatné údaje o účtu.");
 
-        // PUT api/<AccountController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+            byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+            string hashedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password!,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8));
 
-        // DELETE api/<AccountController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            Account accountTemp = new Account()
+            {
+                AccountType = (Enums.AccountType)accountType,
+                Name = name,
+                Surname = surname,
+                SexType = (Enums.SexType)sexType,
+                Email = email,
+                Password = password,
+                Country = country,
+                Region = region,
+                City = city,
+                Address = address,
+                Description = description,
+                Created_At = DateTime.Now.ToUniversalTime(),
+                Salt = salt
+            };
+
+            _context.Accounts.Add(accountTemp);
+            _context.SaveChangesAsync();
+
+            Account registerAccount = _context.Accounts.FirstOrDefault(accountTemp);
+            if (registerAccount == null)
+                return Conflict("Někde nastala chyba.");
+
+            if (registerAccount.AccountType == Enums.AccountType.User)
+            {
+                User userTemp = new User()
+                {
+                    Account = registerAccount,
+                    Date_of_Birth = date
+                };
+                _context.Users.Add(userTemp);
+                _context.SaveChangesAsync();
+            }
+            else if (registerAccount.AccountType == Enums.AccountType.Company)
+            {
+                Company companyTemp = new Company()
+                {
+                    Account = registerAccount,
+                    Founded_At = date
+                };
+                _context.Companies.Add(companyTemp);
+                _context.SaveChangesAsync();
+            }
+            else
+            {
+                return Conflict("Někde nastala chyba.");
+            }
+
+            return Ok("Účet byl úspěšně vytvořen");
         }
     }
 }
--- Projekt/Bemplo/Bemplo.Server/ValidityControl.cs
+++ Projekt/Bemplo/Bemplo.Server/ValidityControl.cs
@@ -1,6 +1,91 @@
-﻿namespace Bemplo.Server
+﻿using Bemplo.Server.Models;
+
+namespace Bemplo.Server
 {
-    public class ValidityControl
+    public static class ValidityControl
     {
+        public static bool CheckNewAccount(byte accountType, string name, string surname, byte sexType, DateTime date, string email, string password, string country, string region, string city, string address, string description, bool agreeWithPrivacyPolicy)
+        {
+            return IsValidAccountType(accountType) &&
+                       IsValidName(name) &&
+                       IsValidSurname(surname) &&
+                       IsValidSexType(sexType) &&
+                       IsValidDate(date) &&
+                       IsValidEmail(email) &&
+                       IsValidPassword(password) &&
+                       IsValidLocation(accountType, country, region, city, address) &&
+                       IsValidDescription(description) &&
+                       IsValidPrivacyPolicyAgreement(agreeWithPrivacyPolicy);
+        }
+
+        private static bool IsValidAccountType(byte accountType)
+        {
+            return Enum.IsDefined(typeof(Enums.AccountType), accountType);
+        }
+
+        private static bool IsValidName(string name)
+        {
+            return !string.IsNullOrEmpty(name) && name.Length <= 50;
+        }
+
+        private static bool IsValidSurname(string surname)
+        {
+            return !string.IsNullOrEmpty(surname) && surname.Length <= 50;
+        }
+
+        private static bool IsValidSexType(byte sexType)
+        {
+            return Enum.IsDefined(typeof(Enums.SexType), sexType);
+        }
+
+        private static bool IsValidDate(DateTime date)
+        {
+            return date <= DateTime.Now;
+        }
+
+        private static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static bool IsValidPassword(string password)
+        {
+            return !string.IsNullOrEmpty(password) && password.Length >= 8;
+        }
+
+        private static bool IsValidLocation(byte accountType, string country, string region, string city, string address)
+        {
+            bool validAddress = false;
+            try
+            {
+                validAddress = (Enums.AccountType)accountType == Enums.AccountType.Company && !string.IsNullOrEmpty(address) || (Enums.AccountType)accountType == Enums.AccountType.User;
+            }
+            catch
+            {
+                validAddress = false;
+            }
+            return !string.IsNullOrEmpty(country) && country.Length <= 50 &&
+                   !string.IsNullOrEmpty(region) && region.Length <= 50 &&
+                   !string.IsNullOrEmpty(city) && city.Length <= 50 &&
+                   !string.IsNullOrEmpty(address) && address.Length <= 100;
+        }
+
+        private static bool IsValidDescription(string description)
+        {
+            return description.Length <= 5000;
+        }
+
+        private static bool IsValidPrivacyPolicyAgreement(bool agreeWithPrivacyPolicy)
+        {
+            return agreeWithPrivacyPolicy;
+        }
     }
 }
